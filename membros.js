8fc3d74e0a271cde55f348f1614dd0898fee73f0
Rodrigo <rodrigoliveira26@gmail.com>
2023-10-19 00:00:51 -0300

    atualização sprint V membros

--- membros.js
+++ membros.js
@@ -1,13 +1,11 @@
-// membrosvalidacao.js
-
 function validarFormulario() {
     var nome = document.getElementById("nome").value;
     var generos = document.getElementById("generos").value;
-    var foto = document.getElementById("foto").value;
+    var fotoInput = document.getElementById("foto"); // Input do arquivo
     var apresentacao = document.getElementById("apresentacao").value;
 
     // Verificar se os campos Nome, Gêneros Literários e Apresentação estão preenchidos
-    if (nome.trim() === "" || generos.trim() === "" || apresentacao.trim() === "") {
+    if (nome.trim() === "" || generos.trim() === "" || apresentacao.trim() === ""|| fotoInput.files.length === 0) {
         alert("Por favor, preencha todos os campos obrigatórios.");
         return false; // Impede o envio do formulário
     }
@@ -26,15 +24,49 @@ function validarFormulario() {
     }
 
     // Verificar se o campo Foto contém uma extensão de arquivo válida
-    if (foto !== "") {
+    if (fotoInput.files.length > 0) {
         var extensoesValidas = ["jpg", "jpeg", "png", "gif"];
-        var extensao = foto.split('.').pop().toLowerCase();
+        var extensao = fotoInput.files[0].name.split('.').pop().toLowerCase();
         if (extensoesValidas.indexOf(extensao) === -1) {
             alert("Apenas arquivos de imagem (jpg, jpeg, png, gif) são permitidos.");
             return false; // Impede o envio do formulário
-        }         
+        }
     }
 
     // Se todas as validações passarem, permite o envio do formulário
-    return true;
-} 
+
+    // Crie um novo elemento de card de membro
+    var memberCard = document.createElement("div");
+    memberCard.classList.add("member-card");
+
+    // Preencha o card com os dados do usuário
+    var fotoURL = URL.createObjectURL(fotoInput.files[0]);
+    memberCard.innerHTML = `
+        <img src="${fotoURL}" alt="Foto de ${nome}">
+        <h2>${nome}</h2>
+        <p>Gêneros Literários Preferidos: ${generos}</p>
+        <div class="social-links">
+            <a href="${facebook}" target="_blank">Facebook</a>
+            <a href="${twitter}" target="_blank">Twitter</a>
+            <a href="${instagram}" target="_blank">Instagram</a>
+        </div>
+        <p>Apresentação: ${apresentacao}</p>
+    `
+
+    // Adicione o novo card ao contêiner
+    var membersContainer = document.getElementById("members-container");
+    membersContainer.appendChild(memberCard);
+
+    // Limpe os campos do formulário após adicionar o membro
+    document.getElementById("nome").value = "";
+    document.getElementById("generos").value = "";
+    fotoInput.value = "";
+    document.getElementById("twitter").value = "";
+    document.getElementById("instagram").value = "";
+    document.getElementById("facebook").value = "";
+    document.getElementById("apresentacao").value = "";
+
+    return false; // Impede o envio do formulário
+}
+
+
